0ddf9a2570f59ce70a10f43db28071b8d80b94f7
matthias <matthias.ae@hotmail.de>
2016-10-22 01:15:17 +0200

    small change

--- Class_Tracer_v2.py
+++ Class_Tracer_v2.py
@@ -30,7 +30,7 @@ plt.close(fig)
 # Graphics
 def figsize(scale):
     '''Give fraction of width, get figsize in inches with height scaled according to golden_mean'''
-    fig_width_pt = 290.74263# 426.79135 #pt 278.83713 #469.755    # Get this from LaTeX using \the\textwidth
+    fig_width_pt = 342.2953 #290.74263# 426.79135 #pt 278.83713 #469.755    # Get this from LaTeX using \the\textwidth
     inches_per_pt = 1.0/72.27                       # Convert pt to inch
     golden_mean = (np.sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this)
     fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
